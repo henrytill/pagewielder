f430b8e17a7f2d21d1e64efc5b8a9b42fea921d4
Henry Till <henrytill@gmail.com>
2023-04-08 23:25:52 +0000

    use command-style CLI, improve

--- pdf_tools/__main__.py
+++ pdf_tools/__main__.py
@@ -1,6 +1,8 @@
 import argparse
+from argparse import Namespace
 import tempfile
 from pathlib import Path
+from typing import Optional
 
 import pikepdf
 
@@ -10,40 +12,37 @@ from pdf_tools import Dimensions, Pages
 
 def user_select_multiple_dimensions(
     dimensions_to_pages_map: dict[Dimensions, Pages]
-) -> set[Dimensions]:
+) -> Optional[set[Dimensions]]:
     """
     Prompt the user to one or more dimensions from a list of dimensions and the
     corresponding number of pages.
     """
-    dimensions_list = list(dimensions_to_pages_map.keys())
-
-    print("Select page dimensions to filter (separated by commas):")
-    for i, dimensions in enumerate(dimensions_list, start=1):
+    dimensions_list = sorted(
+        dimensions_to_pages_map.keys(), key=lambda x: (-x[1], x[0])
+    )
+    print("Available dimensions (width x height) and number of pages:")
+    for i, dimensions in enumerate(dimensions_list):
         width, height = dimensions
         num_pages = len(dimensions_to_pages_map[dimensions])
-        print(
-            "{0}. Dimensions (width x height): {1:8.2f} x {2:8.2f} | Number of pages: {3}".format(
-                i, width, height, num_pages
-            )
+        print(f"{i}: {width:4.2f} x {height:4.2f} ({num_pages} pages)")
+
+    while True:
+        user_input = input(
+            "Select page sets to remove by index (comma-separated) or press Enter to cancel: "
         )
+        if not user_input:
+            return None
+        selected_dimensions = user_input.split(",")
+        try:
+            selected_dimensions_set = {
+                dimensions_list[int(index)] for index in selected_dimensions
+            }
+            return selected_dimensions_set
+        except (ValueError, IndexError):
+            print("Invalid input. Please enter valid indices separated by commas.\n")
 
-    selected_indices = input(
-        "Enter the numbers of the dimensions you want to filter (e.g., '1, 3'): "
-    )
-    selected_indices = [int(idx.strip()) - 1 for idx in selected_indices.split(",")]
-    return {dimensions_list[idx] for idx in selected_indices}
 
-
-def main():
-    parser = argparse.ArgumentParser(
-        description="Filter PDF pages based on dimensions."
-    )
-    parser.add_argument("input", type=Path, help="Path to the input PDF file")
-    parser.add_argument(
-        "-o", "--output", type=Path, help="Path to the output PDF file (optional)"
-    )
-
-    args = parser.parse_args()
+def filter_command(args: Namespace):
     input_path = args.input
 
     if args.output:
@@ -55,17 +54,41 @@ def main():
     dimensions_to_pages_map = pdf_tools.map_page_dimensions_to_pages(input_path)
     selected_dimensions_set = user_select_multiple_dimensions(dimensions_to_pages_map)
 
+    if selected_dimensions_set is None:
+        print("No page sets selected. No output file created.")
+        return
+
     selected_pages = set()
     for dimensions in selected_dimensions_set:
         selected_pages.update(dimensions_to_pages_map[dimensions])
 
-    with pikepdf.open(input_path) as pdf, pikepdf.Pdf.new() as output_pdf:
-        for i, page in enumerate(pdf.pages, start=1):
+    with pikepdf.open(input_path) as input_pdf, pikepdf.Pdf.new() as output_pdf:
+        for i, page in enumerate(input_pdf.pages, start=1):
             if i not in selected_pages:
                 output_pdf.pages.append(page)
         output_pdf.save(output_path)
 
     print(f"Filtered PDF saved as '{output_path}'")
+
+
+def main():
+    parser = argparse.ArgumentParser(description="PDF Tools")
+    subparsers = parser.add_subparsers(help="Commands")
+
+    filter_parser = subparsers.add_parser(
+        "filter", help="Filter PDF pages based on dimensions"
+    )
+    filter_parser.add_argument("input", type=Path, help="Path to the input PDF file")
+    filter_parser.add_argument(
+        "-o", "--output", type=Path, help="Path to the output PDF file (optional)"
+    )
+    filter_parser.set_defaults(func=filter_command)
+
+    args = parser.parse_args()
+    if hasattr(args, "func"):
+        args.func(args)
+    else:
+        parser.print_help()
 
 
 if __name__ == "__main__":
