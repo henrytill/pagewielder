c4affca42e9e0f546a312235966dd24d9331d8ee
Henry Till <henrytill@gmail.com>
2023-06-11 17:07:02 -0700

    restructure

--- pagewielder/__init__.py
+++ pagewielder/__init__.py
@@ -1,21 +1,8 @@
-import collections
+from .core import Dimensions, Pages, map_page_dimensions_to_pages, get_page_dimensions
 
-import pikepdf
-
-Dimensions = tuple[float, float]
-Pages = set[int]
-
-
-def get_page_dimensions(page: pikepdf.Page) -> Dimensions:
-    """Get the dimensions of a page in a PDF file."""
-    rect = pikepdf.Rectangle(page.mediabox)
-    return (rect.width, rect.height)
-
-
-def map_page_dimensions_to_pages(pdf: pikepdf.Pdf) -> dict[Dimensions, Pages]:
-    """Map page dimensions to page numbers."""
-    dimensions_to_pages: dict[Dimensions, Pages] = collections.defaultdict(set)
-    for i, page in enumerate(pdf.pages):
-        dimensions = get_page_dimensions(page)
-        dimensions_to_pages[dimensions].add(i + 1)
-    return dimensions_to_pages
+__all__ = [
+    'Dimensions',
+    'Pages',
+    'map_page_dimensions_to_pages',
+    'get_page_dimensions',
+]
---
+++ pagewielder/core.py
@@ -0,0 +1,21 @@
+import collections
+
+import pikepdf
+
+Dimensions = tuple[float, float]
+Pages = set[int]
+
+
+def get_page_dimensions(page: pikepdf.Page) -> Dimensions:
+    """Get the dimensions of a page in a PDF file."""
+    rect = pikepdf.Rectangle(page.mediabox)
+    return (rect.width, rect.height)
+
+
+def map_page_dimensions_to_pages(pdf: pikepdf.Pdf) -> dict[Dimensions, Pages]:
+    """Map page dimensions to page numbers."""
+    dimensions_to_pages: dict[Dimensions, Pages] = collections.defaultdict(set)
+    for i, page in enumerate(pdf.pages):
+        dimensions = get_page_dimensions(page)
+        dimensions_to_pages[dimensions].add(i + 1)
+    return dimensions_to_pages
